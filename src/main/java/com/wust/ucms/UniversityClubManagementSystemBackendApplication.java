5827df63088a429009ec680034ad30e3f2d79bb7
沙滩上的鱼 <3516464843@qq.com>
2024-02-28 21:39:18 +0800

    feat: build the new project

---
+++ src/main/java/com/wust/ucms/UniversityClubManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wust.ucms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityClubManagementSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityClubManagementSystemBackendApplication.class, args);
+    }
+
+}
