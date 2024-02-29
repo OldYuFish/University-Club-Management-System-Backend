2d5d5543286b0f7087026ec8166d26ec9284c3e0
沙滩上的鱼 <3516464843@qq.com>
2024-03-01 01:10:13 +0800

    feat: build the new project

--- src/main/java/com/wust/ucms/UniversityClubManagementSystemBackendApplication.java
+++ src/main/java/com/wust/ucms/UniversityClubManagementSystemBackendApplication.java
@@ -1,9 +1,15 @@
 package com.wust.ucms;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@EnableScheduling
+@EnableTransactionManagement
+@MapperScan("com.wust.ucms.mapper")
+@SpringBootApplication(scanBasePackages = {"com.wust.ucms"})
 public class UniversityClubManagementSystemBackendApplication {
 
     public static void main(String[] args) {
