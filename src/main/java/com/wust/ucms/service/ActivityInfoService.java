bb42032f6c8722947a964bcdd0f46f2631adc271
沙滩上的鱼 <3516464843@qq.com>
2024-03-03 19:25:41 +0800

    feat: build the new project

--- src/main/java/com/wust/ucms/service/ActivityInfoService.java
+++ src/main/java/com/wust/ucms/service/ActivityInfoService.java
@@ -1,4 +1,15 @@
 package com.wust.ucms.service;
 
+import com.wust.ucms.pojo.ActivityInfo;
+
+import java.util.List;
+import java.util.Map;
+
 public interface ActivityInfoService {
+    Integer createActivityInfo(ActivityInfo activityInfo);
+    Integer deleteActivityInfo(ActivityInfo activityInfo);
+    Integer updateActivityInfo(ActivityInfo activityInfo);
+    ActivityInfo researchDetailActivityInfo(Integer activityId);
+    // TODO 将statusCode作为queryParams的一部分传入，statusCode在controller层赋值，不由前端传递
+    List<Map<String, Object>> researchBasicActivityInfo(ActivityInfo activityInfo);
 }
--- src/main/java/com/wust/ucms/service/impl/ActivityInfoServiceImpl.java
+++ src/main/java/com/wust/ucms/service/impl/ActivityInfoServiceImpl.java
@@ -1,8 +1,42 @@
 package com.wust.ucms.service.impl;
 
+import com.wust.ucms.mapper.ActivityInfoMapper;
+import com.wust.ucms.pojo.ActivityInfo;
 import com.wust.ucms.service.ActivityInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class ActivityInfoServiceImpl implements ActivityInfoService {
+
+    @Autowired
+    ActivityInfoMapper activity;
+
+    @Override
+    public Integer createActivityInfo(ActivityInfo activityInfo) {
+        return null;
+    }
+
+    @Override
+    public Integer deleteActivityInfo(ActivityInfo activityInfo) {
+        return null;
+    }
+
+    @Override
+    public Integer updateActivityInfo(ActivityInfo activityInfo) {
+        return null;
+    }
+
+    @Override
+    public ActivityInfo researchDetailActivityInfo(Integer activityId) {
+        return null;
+    }
+
+    @Override
+    public List<Map<String, Object>> researchBasicActivityInfo(ActivityInfo activityInfo) {
+        return null;
+    }
 }
