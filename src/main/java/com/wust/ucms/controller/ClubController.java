b45e8cbe029a2ea76ecac3ce4a76e4eeea747fca
沙滩上的鱼 <3516464843@qq.com>
2024-03-05 21:02:10 +0800

    feat: build the new project

--- src/main/java/com/wust/ucms/controller/ClubController.java
+++ src/main/java/com/wust/ucms/controller/ClubController.java
@@ -2,16 +2,74 @@ package com.wust.ucms.controller;
 
 import com.wust.ucms.controller.utils.Result;
 import com.wust.ucms.pojo.ClubInfo;
+import com.wust.ucms.service.impl.ClubInfoServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/club")
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class ClubController {
 
+    @Autowired
+    ClubInfoServiceImpl club;
+
+    private static Integer paramsException(ClubInfo clubInfo) {
+        try {
+            if (clubInfo.getClubName() == null || clubInfo.getClubName().isEmpty() ||
+                    clubInfo.getType() == null || clubInfo.getType().isEmpty() ||
+                    clubInfo.getMembersNumber() == null ||
+                    clubInfo.getApplicationTime() == null ||
+                    clubInfo.getLoginId() == null
+            ) throw new Exception("缺少参数！");
+        } catch (Exception e) {
+            return -20001;
+        }
+
+        try {
+            if (clubInfo.getClubName().length() > 24 ||
+                    clubInfo.getMembersNumber() <= 0 ||
+                    clubInfo.getType().length() > 8 ||
+                    clubInfo.getClubLevel().length() > 6 ||
+                    clubInfo.getDepartment().length() > 24 ||
+                    clubInfo.getTotalFund().compareTo(BigDecimal.ZERO) < 0 ||
+                    clubInfo.getTotalFund().compareTo(new BigDecimal("99999999.99")) > 0 ||
+                    clubInfo.getSurplusFund().compareTo(BigDecimal.ZERO) < 0 ||
+                    clubInfo.getSurplusFund().compareTo(new BigDecimal("99999999.99")) > 0
+            ) throw new Exception("参数格式错误！");
+        } catch (Exception e) {
+            return -20002;
+        }
+
+        return 0;
+    }
+
     @PostMapping("/create")
     public Result createClub(@RequestBody ClubInfo clubInfo) {
-        return null;
+        Integer code = paramsException(clubInfo);
+        if (code != 0) return new Result(code);
+
+        try {
+            if (clubInfo.getTotalFund().compareTo(clubInfo.getSurplusFund()) < 0 ||
+                    clubInfo.getEstablishmentTime() != null ||
+                    (clubInfo.getApprovalComment() != null && clubInfo.getApprovalComment().isEmpty()) ||
+                    (clubInfo.getStatusCode() != 0 && clubInfo.getStatusCode() != 1)
+            ) throw new Exception("参数逻辑错误！");
+        } catch (Exception e) {
+            return new Result(-20006);
+        }
+
+        code = club.createClubInfo(clubInfo);
+        if (code <= 0) return new Result(code);
+        Map<String, Object> data = new HashMap<>();
+        data.put("id", code);
+
+        return new Result(0, data);
     }
 
     @PostMapping("/delete")
