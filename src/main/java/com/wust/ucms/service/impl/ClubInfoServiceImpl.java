3cb1e391bb21a3ec907007ea6edd439122f22e3c
沙滩上的鱼 <3516464843@qq.com>
2024-03-05 20:27:17 +0800

    feat: build the new project

--- src/main/java/com/wust/ucms/service/impl/ClubInfoServiceImpl.java
+++ src/main/java/com/wust/ucms/service/impl/ClubInfoServiceImpl.java
@@ -1,5 +1,8 @@
 package com.wust.ucms.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.wust.ucms.mapper.ClubInfoMapper;
 import com.wust.ucms.mapper.LoginInfoMapper;
 import com.wust.ucms.pojo.ClubInfo;
@@ -10,8 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
 import java.math.BigDecimal;
-import java.util.Date;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class ClubInfoServiceImpl implements ClubInfoService {
@@ -94,6 +96,52 @@ public class ClubInfoServiceImpl implements ClubInfoService {
 
     @Override
     public Map<String, Object> researchBasicClubInfo(ClubInfo clubInfo) {
-        return null;
+        LambdaQueryWrapper<ClubInfo> lqw = new LambdaQueryWrapper<>();
+        lqw.like(
+                clubInfo.getClubName() != null && !clubInfo.getClubName().isEmpty(),
+                ClubInfo::getClubName,
+                clubInfo.getClubName()
+        ).eq(
+                clubInfo.getType() != null && !clubInfo.getType().isEmpty(),
+                ClubInfo::getType,
+                clubInfo.getType()
+        ).eq(
+                clubInfo.getClubLevel() != null && !clubInfo.getClubLevel().isEmpty(),
+                ClubInfo::getClubLevel,
+                clubInfo.getClubLevel()
+        ).like(
+                clubInfo.getDepartment() != null && !clubInfo.getDepartment().isEmpty(),
+                ClubInfo::getDepartment,
+                clubInfo.getDepartment()
+        ).eq(
+                clubInfo.getStatusCode() != null,
+                ClubInfo::getStatusCode,
+                clubInfo.getStatusCode()
+        );
+
+        IPage<ClubInfo> page = new Page<>(clubInfo.getPageIndex(), clubInfo.getPageSize());
+        club.selectPage(page, lqw);
+
+        Map<String, Object> pagination = new HashMap<>();
+        pagination.put("total", page.getTotal());
+        pagination.put("pageIndex", clubInfo.getPageIndex());
+        pagination.put("pageSize", clubInfo.getPageSize());
+
+        List<Map<String, Object>> clubList = new ArrayList<>();
+        for (ClubInfo c: page.getRecords()) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("clubName", c.getClubName());
+            map.put("type", c.getType());
+            map.put("clubLevel", c.getClubLevel());
+            map.put("department", c.getDepartment() == null || c.getDepartment().isEmpty() ? "--" : c.getDepartment());
+            map.put("statusCode", c.getStatusCode());
+            map.put("realName", login.selectById(c.getLoginId()).getRealName());
+            clubList.add(map);
+        }
+        Map<String, Object> data = new HashMap<>();
+        data.put("clubList", clubList);
+        data.put("pagination", pagination);
+
+        return data;
     }
 }
