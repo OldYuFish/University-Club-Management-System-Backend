340c29b0c25e4ce837dd6e2bb78f404f28e97cae
沙滩上的鱼 <3516464843@qq.com>
2024-03-10 11:10:25 +0800

    feat: build the new project

--- src/main/java/com/wust/ucms/controller/LogController.java
+++ src/main/java/com/wust/ucms/controller/LogController.java
@@ -6,6 +6,8 @@ import com.wust.ucms.service.impl.LogServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/log")
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -16,6 +18,27 @@ public class LogController {
 
     @PostMapping("/research")
     public Result researchLog(@RequestBody Log log) {
-        return null;
+        try {
+            if (log.getPageIndex() == null ||
+                    log.getPageSize() == null
+            ) throw new Exception("缺少参数！");
+        } catch (Exception e) {
+            return new Result(-20001);
+        }
+
+        try {
+            if (log.getObject().length() > 12 ||
+                    log.getOperate().length() > 6 ||
+                    ((log.getStudentNumber() != null && !log.getStudentNumber().isEmpty()) &&
+                            log.getStudentNumber().length() != 12) ||
+                    log.getTeacherNumber().length() > 24
+            ) throw new Exception("参数格式错误！");
+        } catch (Exception e) {
+            return new Result(-20002);
+        }
+
+        Map<String, Object> data = logging.researchLog(log);
+
+        return new Result(0, data);
     }
 }
